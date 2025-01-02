8d67558e3c16443c17eea8fcf59048a9731b3b72
NuwanSrajapasha <s22010099@ousl.lk>
2025-01-02 07:30:49 +0000

    search customer and print details

--- src/edu/ijse/mvc/model/orderModel.java
+++ src/edu/ijse/mvc/model/orderModel.java
@@ -5,15 +5,73 @@
 package edu.ijse.mvc.model;
 
 import edu.ijse.mvc.db.DBconnection;
+import edu.ijse.mvc.dto.OrderDetailDto;
 import edu.ijse.mvc.dto.orderDto;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import edu.ijse.mvc.dto.IthemDto;
+
 
 /**
  *
  * @author HP
  */
 public class orderModel {
+    
+     private OrderDetailModel orderDetailModel = new OrderDetailModel();
+    private ItemModel itemModel = new ItemModel();
+    
+    public String placeOrder(orderDto orderDto, ArrayList<OrderDetailDto> orderDetailDtos) throws Exception{
+        Connection connection = DBconnection.getInstance().getConnection(); 
+        try {
+            connection.setAutoCommit(false);
+            String orderSaveResp = saveOrder(orderDto);
+            if(orderSaveResp.equalsIgnoreCase("Success")){
+                boolean isOrderDetailSaved = true;
+                for (OrderDetailDto orderDetailDto : orderDetailDtos) {
+                    String orderDetailRsp = orderDetailModel.saveOrderDetail(orderDetailDto);
+                    if(!orderDetailRsp.equalsIgnoreCase("Success")){
+                        isOrderDetailSaved = false;
+                    }
+                }
+                if(isOrderDetailSaved){
+                    boolean isItemUpdate = true;
+                    for (OrderDetailDto orderDetailDto : orderDetailDtos) {
+                        IthemDto itemDto = itemModel.searchItem(orderDetailDto.getItemId());
+                        if(itemDto != null){
+                            itemDto.setQoh(itemDto.getQoh() - orderDetailDto.getQty());
+                            String resp = itemModel.updateItem(itemDto);
+                            if(!resp.equalsIgnoreCase("Successfully Updated")){
+                                isItemUpdate = false;
+                            }
+                        } else {
+                            connection.rollback();
+                            return "Item Not Found";
+                        }
+                    }
+                    if(isItemUpdate){
+                        connection.commit();
+                        return "Success";
+                    } else {
+                        connection.rollback();
+                        return "Item Update Error";
+                    }
+                } else {
+                    connection.rollback();
+                    return "Order Detail Save Error";
+                }
+            } else {
+                connection.rollback();
+                return "Order Save Error";
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            connection.setAutoCommit(true);
+        }
+    }
+    
     public String saveOrder(orderDto OrderDto) throws Exception{
         String sql = "INSERT INTO Orders VALUES (?,?,?)";
         Connection connection = DBconnection.getInstance().getConnection(); 
